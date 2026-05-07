855ba1bccf6e40d5f1b88558c5187d2f858484e6
malik9594 <hassanrazamalik512@gmail.com>
2026-05-07 11:15:13 +0500

    icon changes in website

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,28 +40,28 @@ export const metadata: Metadata = {
     title: "Top Google Maps & Mapbox Alternative in USA | MapifyIt",
     description:
       "Looking for Google Maps & Mapbox alternative in the USA? MapifyIt offers GIS mapping, unlimited APIs, & enterprise-grade solutions tailored for businesses.",
-    // images: [
-    //   {
-    //     url: "/fullwhitebackground logo.png",
-    //     width: 1200,
-    //     height: 630,
-    //     alt: "Mapifyit – Enterprise Location Intelligence",
-    //   },
-    // ],
+    images: [
+      {
+        url: "/fullwhite-background.png",
+        width: 1200,
+        height: 630,
+        alt: "Mapifyit – Enterprise Location Intelligence",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Top Google Maps & Mapbox Alternative in USA | MapifyIt",
     description:
       "Looking for Google Maps & Mapbox alternative in the USA? MapifyIt offers GIS mapping, unlimited APIs, & enterprise-grade solutions tailored for businesses.",
-    // images: ["/fullwhitebackground logo.png"],
+    images: ["/fullwhite-background.png"],
     creator: "@mapifyit",
   },
   icons: {
     icon: [
-      // { url: "/fullwhitebackground logo.png", sizes: "16x16", type: "image/png" },
-      // { url: "/fullwhitebackground logo.png", sizes: "32x32", type: "image/png" },
-      // { url: "/fullwhitebackground logo.png", sizes: "192x192", type: "image/png" },
+      { url: "/fullwhite-background.png", sizes: "16x16", type: "image/png" },
+      { url: "/fullwhite-background.png", sizes: "32x32", type: "image/png" },
+      { url: "/fullwhite-background.png", sizes: "192x192", type: "image/png" },
     ],
     // apple: "/fullwhitebackground logo.png",
   },
